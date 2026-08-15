996bfa90dbbea163b75d80f75b00b4a75b9a8ef0
Fercho <fernandoaltamirano.vdc@gmail.com>
2026-08-15 09:35:50 -0600

    gui

--- Laboratorio4Progra/src/laboratorio4progra/Laboratorio4Progra.java
+++ Laboratorio4Progra/src/laboratorio4progra/Laboratorio4Progra.java
@@ -1,9 +1,12 @@
+package laboratorio4progra;
+import javax.swing.SwingUtilities;
+import laboratorio4progra.VentanaPrincipal;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author David Suazo Palao
@@ -14,7 +17,8 @@ public class Laboratorio4Progra {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        SwingUtilities.invokeLater(() -> new VentanaPrincipal().setVisible(true));
     }
-    
+
 }
