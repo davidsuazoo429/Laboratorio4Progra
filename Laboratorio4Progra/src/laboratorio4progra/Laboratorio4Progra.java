9344e7a0faead4e324d5dffd05a3d27485b47f2b
David Suazo <dsuazo@gmail.com>
2026-08-15 08:54:03 -0600

    base

---
+++ Laboratorio4Progra/src/laboratorio4progra/Laboratorio4Progra.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author David Suazo Palao
+ */
+public class Laboratorio4Progra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
